bbab3dd1cbb9954856880e4d5cd509b18401e13f
Shirley Moore <mooresv@ornl.gov>
2021-01-19 15:13:35 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ g++ -fopenmp -o main.exe src/main.cpp src/utils.cpp src/sort1.cpp src/sort2.cpp 
 mkdir results;
 
 # Make experiments
-for ALGORITHM in 1 3
+for ALGORITHM in 3
 do
-	for BUCKETS in 5 10 15 20 25
+	for BUCKETS in 8 16 32 48 64
 	do
 		for THREADS in {1..8}
 		do 
